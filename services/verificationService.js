dd5c5849e0e8402c36795ffc57e6f72713a579b5
Andrew Lischuk <andrewlischuk59@gmail.com>
2023-03-18 09:17:27 +0200

    fixed re-verification

--- services/verificationService.js
+++ services/verificationService.js
@@ -1,10 +1,10 @@
 const { User } = require("../db/userModel");
-const path = require("path");
 const {
   VerificationError,
   WrongParametersError,
 } = require("../helpers/errors");
-const { transporter } = require("../helpers/mailSender");
+
+const { PORT, BASE_URL } = process.env;
 
 const verification = async (providedToken) => {
   const user = await User.findOne({ verificationToken: providedToken });
@@ -19,25 +19,14 @@ const repeatVerification = async (email) => {
   if (user.verify) {
     throw new WrongParametersError("Verification has already been passed");
   }
-  console.log(user.verificationToken);
 
-  const verifyURL = path.join(
-    `localhost:${process.env.PORT}`,
-    "/api/auth/users",
-    `/verify/${user.verificationToken}`
-  );
-  const emailOptions = {
-    from: "andrewlischuk@meta.ua",
+  const verificationOptions = {
     to: email,
-    subject: "Repeat verification",
+    subject: "Repeate verification",
     text: "Hello! Please verify your email by clicking the link below!",
-    html: `<a target="_blank" href="http://${verifyURL}">Confirm your email</a>`,
+    html: `<a target="_blank" href="${BASE_URL}${PORT}/api/auth/users/verify/${user.verificationToken}">Confirm your email</a>`,
   };
-
-  transporter
-    .sendMail(emailOptions)
-    .then(() => {})
-    .catch((err) => console.log(err));
+  await sendEmail(verificationOptions);
 };
 
 module.exports = { verification, repeatVerification };
